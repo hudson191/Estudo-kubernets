1d3becf350b63a4154ce319e54aa8acf298b369f
Hudson Von Zuben de Oliveira <hudson@atmtec.com.br>
2022-01-05 18:03:43 -0300

    Healthcheck

--- go-hello/server.go
+++ go-hello/server.go
@@ -1,10 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var startedAt = time.Now()
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/healthz", Health)
 	http.ListenAndServe(":80", nil)
+}
+
+func Health(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("OK %v", duration.Seconds())))
+	}
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
